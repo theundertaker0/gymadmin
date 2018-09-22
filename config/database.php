bec7bd14c941bc69aa11ad307100853980a1a3a4
juanesquivelmendez <amadeusdigitalmid@gmail.com>
2018-09-22 15:36:30 -0500

    cambios menores

--- config/database.php
+++ config/database.php
@@ -41,7 +41,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'bd4free.net'),
+            'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'gymadmindb'),
             'username' => env('DB_USERNAME', 'gofattyadmin'),
